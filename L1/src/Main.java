5fc36fa4a80c49e3d26cba7d27a35c4942a283f2
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-10-27 20:11:07 +0100

    Make visualization disabable by flag

--- L1/src/Main.java
+++ L1/src/Main.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static boolean SHOW_AST_VISUALIZATION = false;
+
     public static void main(String[] args) throws Exception {
         var lexer = new L1Lexer(CharStreams.fromReader(new StringReader("(17+4)*2*1")));
         var parser = new L1Parser(new CommonTokenStream(lexer));
@@ -17,16 +19,19 @@ public class Main {
         ParseTreeWalker.DEFAULT.walk(new Eval(), tree);
         System.out.println(tree.result);
 
-        // UI AST Visualization
-        JFrame frame = new JFrame("Antlr AST");
-        JPanel panel = new JPanel();
-        TreeViewer viewer = new TreeViewer(Arrays.asList(
-                parser.getRuleNames()), tree);
-        viewer.setScale(1.5); // Scale a little
-        panel.add(viewer);
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        if (SHOW_AST_VISUALIZATION) {
+            // UI AST Visualization
+            JFrame frame = new JFrame("Antlr AST");
+            JPanel panel = new JPanel();
+            TreeViewer viewer = new TreeViewer(Arrays.asList(
+                    parser.getRuleNames()), tree);
+            viewer.setScale(1.5); // Scale a little
+            panel.add(viewer);
+            frame.add(panel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
+    }
     }
 }
