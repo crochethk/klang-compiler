7e1bc50f918a325ee46fb5733d608b3cf11d13b9
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-11-04 19:44:03 +0100

    Add `Prog` Node class

---
+++ p3/src/cc/crochethk/compilerbau/p3/ast/Prog.java
@@ -0,0 +1,19 @@
+package cc.crochethk.compilerbau.p3.ast;
+
+import java.util.List;
+
+import cc.crochethk.compilerbau.p3.Visitor;
+
+public class Prog extends Node {
+    public List<FunDef> funDefs;
+
+    public Prog(int line, int column, List<FunDef> funDefs) {
+        super(line, column);
+        this.funDefs = funDefs;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) throws Exception {
+        return visitor.visit(this);
+    }
+}
