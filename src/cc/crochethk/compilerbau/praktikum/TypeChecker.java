ba2b21ff8f56d0cc864c190a35a96c0e4e774abc
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-11-19 02:20:27 +0100

    Adapt remaining visit methods (except FunCall)

--- src/cc/crochethk/compilerbau/praktikum/TypeChecker.java
+++ src/cc/crochethk/compilerbau/praktikum/TypeChecker.java
@@ -156,17 +156,6 @@ public class TypeChecker implements Visitor<Void> {
 
     @Override
     public Void visit(VarAssignStat varAssignStat) {
-        /*
-        [x] 0. Make sure local variables are somehow tracked in the visitor (e.g. Map<String, Type>)
-        [x] 1. lookup the type of "targetVar" in the local variables stack (or whatever) using "var.name"
-            2. if lookup...
-            [x] 2.1 ...succeeded: "targetVar" is declared
-                    -> Check compatibility of "expr.theType" and "targetVarType"
-                [x]     - fail: reportError "Assignment of incompatible type"
-                [x]     - success: Assign "varAssignStat.theType = targetVar.theType" (or  ""...= expr.theType")
-            [x] 2.2 ...failed: report Error "Assignment to undeclared variable"
-        */
-
         varAssignStat.expr.accept(this);
 
         var varType = funDefVarTypes.get(varAssignStat.targetVarName);
@@ -177,10 +166,10 @@ public class TypeChecker implements Visitor<Void> {
         } else if (!(varType.equals(exprType))) {
             reportError(varAssignStat, "Attempt to assign value of type '"
                     + exprType + "' to variable '" + varAssignStat.targetVarName
-                    + "' of incompatible type '" + varType + "')");
+                    + "' of incompatible type '" + varType + "'");
         }
 
-        varAssignStat.theType = exprType;
+        varAssignStat.theType = varType;
         return null;
     }
 
@@ -207,20 +196,8 @@ public class TypeChecker implements Visitor<Void> {
     @Override
     public Void visit(StatementListNode statementListNode) {
         statementListNode.value.accept(this);
-        // statementListNode.next.accept(this);
-        /*
-        TODO TODO TODO TODO TODO
-        - to consider:
-            - check that all code paths (one or more return statements) produce a similar type
-            - multiple return statements in the list
-                - "how to track and check this?"
-                    - create new list of types on funDef visit
-                    - collect each returnStat type into this list
-                    - in fundef: do type ckecks with this list after evaluation the funDef.body
-                    - empty list when done, so next funDef can start with fresh context
-                - checking if all return compatible type
-            - I believe interpreter already had a solution to the multi-return problem
-        */
+        statementListNode.next.accept(this);
+        statementListNode.theType = Type.VOID_T;
         return null;
     }
 
@@ -229,27 +206,19 @@ public class TypeChecker implements Visitor<Void> {
         ifElseStat.condition.accept(this);
         ifElseStat.then.accept(this);
         ifElseStat.otherwise.accept(this);
+        var condType = ifElseStat.condition.theType;
 
-        /*
-        TODO TODO TODO TODO TODO 
-        - see ternaryNode and interpreter visitor impl about result typing?
-            - maybe something along the lines:
-                - check "then.theType == otherwise.theType"
-                - check whether "condition.theType == BoolT"
-                - set: ifElseStat.theType = then.theType
-        */
+        if (!condType.equals(Type.BOOLEAN_T)) {
+            reportError(ifElseStat.condition,
+                    "Condition must evaluate to boolean but is '" + condType + "'");
+        }
+
+        ifElseStat.theType = ifElseStat.then.theType;
         return null;
     }
 
     @Override
     public Void visit(Var var) {
-        /*
-        [x] 0. Make sure local variables are somehow tracked in the visitor (e.g. Map<String, Type>)
-        [x] 1. lookup the type of "var" in the local variables stack (or whatever) using "var.name"
-            2. if lookup...
-            [x] 2.1 ...succeeded: "var" is declared -> set "var.theType = varType"
-            [x] 2.2 ...failed: report Error "Use of undefined variable '" + var.name + "'"
-        */
         var varType = funDefVarTypes.get(var.name);
         if (varType == null) {
             reportError(var, "Use of undefined variable '" + var.name + "'");
@@ -273,9 +242,19 @@ public class TypeChecker implements Visitor<Void> {
 
     @Override
     public Void visit(UnaryOpExpr unaryOpExpr) {
-        //TODO maybe a check should be done, whether the operator is applicable to the operand type
         unaryOpExpr.operand.accept(this);
-        unaryOpExpr.theType = unaryOpExpr.operand.theType;
+        var operandType = unaryOpExpr.operand.theType;
+        unaryOpExpr.theType = operandType;
+        var op = unaryOpExpr.op;
+
+        if (op.isBoolean() && operandType.isNumeric()) {
+            reportError(unaryOpExpr, "Boolean operator incompatible with '"
+                    + operandType + "' operand");
+        }
+        if (op.isArithmetic() && !operandType.isNumeric()) {
+            reportError(unaryOpExpr, "Arithmetic operator '" + op
+                    + "' incompatible with '" + operandType + "' operand");
+        }
         return null;
     }
 
@@ -290,23 +269,23 @@ public class TypeChecker implements Visitor<Void> {
         ternaryConditionalExpr.theType = thenType;
 
         if (!thenType.equals(otherwiseType)) {
-            reportError(ternaryConditionalExpr, "Conditional branches return different types");
+            reportError(ternaryConditionalExpr.then, "Conditional branches return incompatible types");
         }
-        if (!(condType instanceof BoolT)) {
-            reportError(ternaryConditionalExpr, "Condition must return a boolean type");
+        if (!condType.equals(Type.BOOLEAN_T)) {
+            reportError(ternaryConditionalExpr.condition, "Condition must return a boolean type");
         }
         return null;
     }
 
     @Override
     public Void visit(EmptyNode emptyNode) {
-        emptyNode.theType = createVoidT(emptyNode.line, emptyNode.column);
+        emptyNode.theType = Type.VOID_T;
         return null;
     }
 
     @Override
     public Void visit(TypeNode type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        type.theType = Type.of(type.typeToken, "" /*default package */);
+        return null;
     }
 }
