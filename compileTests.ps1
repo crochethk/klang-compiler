a7f24c77c1797143f549aafbf22eb6c250996360
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-11-22 15:54:14 +0100

    Improve helper powershell scripts

--- compileTests.ps1
+++ compileTests.ps1
@@ -1,5 +1,5 @@
 # import helper functions
-. .\loadEnvVars.ps1
+. .\utils.ps1
 
 # Load environment variables from file
 $envFilePath = ".\L1Compiler.env"
@@ -8,6 +8,5 @@ LoadEnvVariables -filePath $envFilePath
 # Compiles all "*.l1" files of "./tests" into "gen_jbc"
 $testsSourcesDir = $env:TESTS_DIR
 Get-ChildItem -Path $testsSourcesDir -Include "*.l1", "*.L1" -Recurse | ForEach-Object {
-    $relativePath = $_.FullName.Substring($pwd.Path.Length + 1)  # Strip out the base directory part
-    java --enable-preview -cp "bin;lib/antlr4-runtime-4.13.2.jar;lib/antlr4-4.13.2.jar;lib/antlr4-4.13.2-complete.jar" L1Compiler $env:OUTDIR $relativePath
+    CompileFile -outDir $env:OUTDIR -sourceFilePath $_.FullName
 }
