464a02ad70d3d5c69b870eb05e32e32b5baab598
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-04 06:10:23 +0100

    Refactor write helpers into AsmCodeWriter

--- src/main/java/cc/crochethk/compilerbau/praktikum/GenAsm.java
+++ src/main/java/cc/crochethk/compilerbau/praktikum/GenAsm.java
@@ -12,17 +12,13 @@ import java.util.Map;
 
 import cc.crochethk.compilerbau.praktikum.ast.*;
 import cc.crochethk.compilerbau.praktikum.ast.literals.*;
+import cc.crochethk.compilerbau.praktikum.GenAsm.AsmCodeWriter;
 import utils.Result;
 
-public class GenAsm extends CodeGenVisitor<Writer> {
+public class GenAsm extends CodeGenVisitor<AsmCodeWriter> {
     private static final String FILE_EXT = ".s";
 
-    @Override
-    public String outFilePath() {
-        return Path.of(outDir, packageName + "." + className + FILE_EXT).toString();
-    }
-
-    Writer writer;
+    AsmCodeWriter acw;
 
     /** Function argument registers */
     private final Register[] regs = { rdi, rsi, rdx, rcx, r8, r9 };
@@ -34,144 +30,119 @@ public class GenAsm extends CodeGenVisitor<Writer> {
         var parentDir = Path.of(filePath).getParent();
         parentDir = parentDir != null ? parentDir : Path.of("");
         Files.createDirectories(parentDir);
-        this.writer = new FileWriter(filePath);
-    }
-
-    /** Write multiple strings into new indented line */
-    private Writer writeIndented(Object... ss) {
-        write("\n\t");
-        for (var s : ss)
-            write(s);
-        return writer;
-    }
-
-    /** Write the string into new indented line */
-    private Writer writeIndented(Object s) {
-        return write("\n\t", s);
-    }
-
-    private Writer write(Object... ss) {
-        for (var s : ss)
-            write(s.toString());
-        return writer;
-    }
-
-    private Writer write(Object s) {
-        try {
-            writer.write(s.toString());
-        } catch (IOException e) {
-            exitStatus = Result.Err;
-            throw new RuntimeException(e);
-        }
-        return writer;
+        this.acw = new AsmCodeWriter(new FileWriter(filePath));
     }
 
     @Override
-    public Writer visit(I64Lit i64Lit) {
-        return writeIndented("movq", "\t", "$" + i64Lit.value, ", ", rax);
+    public String outFilePath() {
+        return Path.of(outDir, packageName + "." + className + FILE_EXT).toString();
     }
 
     @Override
-    public Writer visit(F64Lit f64Lit) {
+    public AsmCodeWriter visit(I64Lit i64Lit) {
+        return acw.movq("$" + i64Lit.value, rax);
+    }
+
+    @Override
+    public AsmCodeWriter visit(F64Lit f64Lit) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(BoolLit boolLit) {
+    public AsmCodeWriter visit(BoolLit boolLit) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(StringLit stringLit) {
+    public AsmCodeWriter visit(StringLit stringLit) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(Var var) {
-        return writeIndented(
-                "movq", "\t", ctx.get(var.name).toString() + "(" + rbp + "), ", rax);
+    public AsmCodeWriter visit(Var var) {
+        return acw.movq(ctx.get(var.name).toString() + "(" + rbp + ")", rax);
     }
 
     @Override
-    public Writer visit(FunCall funCall) {
+    public AsmCodeWriter visit(FunCall funCall) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(BinOpExpr binOpExpr) {
+    public AsmCodeWriter visit(BinOpExpr binOpExpr) {
         // TODO use actual operators
         // TODO hardcoded ADD for now!
         binOpExpr.lhs.accept(this);
-        writeIndented("movq", "\t", rax, ", ", rdx);
+        acw.movq(rax, rdx);
         binOpExpr.rhs.accept(this);
-        return writeIndented("addq", "\t", rdx, ", ", rax);
+        return acw.writeIndented("addq", "\t", rdx, ", ", rax);
     }
 
     @Override
-    public Writer visit(UnaryOpExpr unaryOpExpr) {
+    public AsmCodeWriter visit(UnaryOpExpr unaryOpExpr) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(TernaryConditionalExpr ternaryConditionalExpr) {
+    public AsmCodeWriter visit(TernaryConditionalExpr ternaryConditionalExpr) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(VarDeclareStat varDeclareStat) {
+    public AsmCodeWriter visit(VarDeclareStat varDeclareStat) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(VarAssignStat varAssignStat) {
+    public AsmCodeWriter visit(VarAssignStat varAssignStat) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(IfElseStat ifElseStat) {
+    public AsmCodeWriter visit(IfElseStat ifElseStat) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(LoopStat loopStat) {
+    public AsmCodeWriter visit(LoopStat loopStat) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(StatementList statementList) {
+    public AsmCodeWriter visit(StatementList statementList) {
         statementList.statements.forEach(s -> s.accept(this));
-        return writer;
+        return acw;
     }
 
     @Override
-    public Writer visit(ReturnStat returnStat) {
+    public AsmCodeWriter visit(ReturnStat returnStat) {
         returnStat.expr.accept(this);
         /* Epilogue */
         // Restore caller's context:
         // -> Copy %rbp to %rsp and then replace %rbp with the stored value
         // -> Can be reduced to "popq" part in certain cases.
-        writeIndented("leave");
-        return writeIndented("ret");
+        acw.writeIndented("leave");
+        return acw.writeIndented("ret");
     }
 
     @Override
-    public Writer visit(BreakStat breakStat) {
+    public AsmCodeWriter visit(BreakStat breakStat) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Writer visit(TypeNode type) {
+    public AsmCodeWriter visit(TypeNode type) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -183,25 +154,24 @@ public class GenAsm extends CodeGenVisitor<Writer> {
     Map<String, Integer> ctx = null;
 
     @Override
-    public Writer visit(FunDef funDef) {
+    public AsmCodeWriter visit(FunDef funDef) {
         var oldCtx = ctx;
         ctx = new HashMap<>();
 
-        writeIndented(".globl\t", funDef.name);
-        writeIndented(".type\t", funDef.name, ", @function");
-        write("\n", funDef.name, ":");
+        acw.writeIndented(".globl\t", funDef.name);
+        acw.writeIndented(".type\t", funDef.name, ", @function");
+        acw.write("\n", funDef.name, ":");
 
         /* Prologue */
         // Backup caller's and set callee's context
-        writeIndented("pushq", "\t", rbp);
-        writeIndented("movq", "\t", rsp + ", " + rbp);
-
+        acw.writeIndented("pushq", "\t", rbp);
+        acw.movq(rsp, rbp);
         var regParamsCount = Math.min(funDef.params.size(), regs.length);
         var baseoffset = 0;
         for (int i = 0; i < regParamsCount; i++) {
             baseoffset -= 8;
             ctx.put(funDef.params.get(i).name(), baseoffset);
-            writeIndented("movq", "\t", regs[i], ", ", baseoffset + "(" + rbp + ")");
+            acw.movq(regs[i], baseoffset + "(" + rbp + ")");
         }
 
         // when >6 params: get offsets of caller-saved params
@@ -219,32 +189,76 @@ public class GenAsm extends CodeGenVisitor<Writer> {
         if (funDef.body.isEmpty()
                 || (!funDef.body.statements.getLast().returnsControlFlow()
                         && funDef.returnType.theType.equals(Type.VOID_T))) {
-            writeIndented("leave");
-            writeIndented("ret");
+            acw.writeIndented("leave");
+            acw.writeIndented("ret");
         }
 
         ctx = oldCtx;
-        return writer;
+        return acw;
     }
 
     @Override
-    public Writer visit(Prog prog) {
+    public AsmCodeWriter visit(Prog prog) {
         exitStatus = Result.Ok;
 
         prog.funDefs.forEach(f -> f.accept(this));
-        writeIndented(".section\t.note.GNU-stack,\"\",@progbits");
-        write("\n");
+        acw.writeIndented(".section\t.note.GNU-stack,\"\",@progbits", "\n");
         try {
-            writer.close();
+            acw.close();
         } catch (IOException e) {
             System.err.println(e);
             exitStatus = Result.Err;
         }
-        return writer;
+        return acw;
     }
 
     @Override
-    public Writer visit(EmptyNode emptyNode) {
-        return writer;
+    public AsmCodeWriter visit(EmptyNode emptyNode) {
+        return acw;
+    }
+
+    class AsmCodeWriter {
+        private Writer writer;
+
+        AsmCodeWriter(Writer writer) {
+            this.writer = writer;
+        }
+
+        AsmCodeWriter movq(Object source, Object target) {
+            return writeIndented("movq", "\t", source.toString(), ", ", target.toString());
+        }
+
+        /** Write multiple strings into new indented line */
+        AsmCodeWriter writeIndented(Object... ss) {
+            write("\n\t");
+            for (var s : ss)
+                write(s);
+            return this;
+        }
+
+        /** Write the string into new indented line */
+        AsmCodeWriter writeIndented(Object s) {
+            return write("\n\t", s);
+        }
+
+        AsmCodeWriter write(Object... ss) {
+            for (var s : ss)
+                write(s.toString());
+            return this;
+        }
+
+        AsmCodeWriter write(Object s) {
+            try {
+                writer.write(s.toString());
+            } catch (IOException e) {
+                exitStatus = Result.Err;
+                throw new RuntimeException(e);
+            }
+            return this;
+        }
+
+        void close() throws IOException {
+            writer.close();
+        }
     }
 }
