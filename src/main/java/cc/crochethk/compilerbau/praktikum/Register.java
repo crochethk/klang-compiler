8a49d1465c00838efe6e9585fb275bb381341a37
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-06 03:43:07 +0100

    GenAsm: simplify Register enum

--- src/main/java/cc/crochethk/compilerbau/praktikum/Register.java
+++ src/main/java/cc/crochethk/compilerbau/praktikum/Register.java
@@ -2,31 +2,10 @@ package cc.crochethk.compilerbau.praktikum;
 
 /** Enum representing registers according to x86-64, Linux System V ABI */
 public enum Register {
-    rax("%rax"),
-    rcx("%rcx"),
-    rdx("%rdx"),
-    rbx("%rbx"),
-    rsi("%rsi"),
-    rdi("%rdi"),
-    rsp("%rsp"),
-    rbp("%rbp"),
-    r8("%r8"),
-    r9("%r9"),
-    r10("%r10"),
-    r11("%r11"),
-    r12("%r12"),
-    r13("%r13"),
-    r14("%r14"),
-    r15("%r15");
-
-    String name;
-
-    Register(String regName) {
-        name = regName;
-    }
+    rax, rcx, rdx, rbx, rsi, rdi, rsp, rbp, r8, r9, r10, r11, r12, r13, r14, r15;
 
     @Override
     public String toString() {
-        return name;
+        return "%" + super.toString();
     }
 }
