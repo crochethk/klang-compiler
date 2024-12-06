b6aa7d557f955532965ae3fae5bcf0a1325429fb
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-06 05:05:47 +0100

    GenAsm: Implement constant and memory address OperandSpecifiers

--- src/main/java/cc/crochethk/compilerbau/praktikum/OperandSpecifier.java
+++ src/main/java/cc/crochethk/compilerbau/praktikum/OperandSpecifier.java
@@ -12,4 +12,61 @@ public interface OperandSpecifier {
             return "%" + this;
         }
     }
+
+    public record Const(long value) implements OperandSpecifier {
+        /** Alias for new Const(value) */
+        public static Const $(long value) {
+            return new Const(value);
+        }
+
+        @Override
+        public String operandSpec() {
+            return "$" + value();
+        }
+    }
+
+    public record RawMemAddr(int address) implements OperandSpecifier {
+        @Override
+        public String operandSpec() {
+            return address() + "";
+        }
+    }
+
+    /** Memory Address using the "<offset>(<base>, <index>, <scale>)" operand syntax */
+    public class MemAddr implements OperandSpecifier {
+        private int offset;
+        private Register base, index;
+        private Scale s;
+
+        public MemAddr(int offset, Register base) {
+            this(offset, base, null, null);
+        }
+
+        public MemAddr(int offset, Register base, Register index, Scale s) {
+            this.offset = offset;
+            this.base = base;
+            this.index = index;
+            this.s = s;
+        }
+
+        public enum Scale {
+            _1(1), _2(2), _4(4), _8(8);
+
+            final int factor;
+
+            Scale(int f) {
+                this.factor = f;
+            }
+        }
+
+        @Override
+        public String operandSpec() {
+            // return offset + "(" + base.operandSpec() + "," + index.operandSpec() + "," + s.factor + ")";
+            return offset + "(" + base.operandSpec()
+                    + (index != null
+                            ? "," + index.operandSpec() + "," + s.factor
+                            : "")
+                    + ")";
+        }
+    }
 }
