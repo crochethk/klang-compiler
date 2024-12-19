e786bc1d1ff166ad83788a76d3198f8d787c5030
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-19 23:03:57 +0100

    Add run_antlr script and move shell scripts to own folder

--- scripts/build_compiler_jar.sh
+++ scripts/build_compiler_jar.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+# >>> Expects $(pwd) to be the __project root folder__. <<<
 
 # ==============================================================================
-# This script builds a self-contained JAR of the klang compiler.
+# Builds a self-contained JAR of the klang compiler.
 #
-# To run it using configuration from a ".env" or with the defaults type:
-#       java --enable-preview -jar klangc.jar
+# Jar Usage: java --enable-preview -jar klangc.jar [OUTDIR SOURCEFILE [FILES...]]
+# - [OUTDIR SOURCEFILE [FILES...]]
+#       Semi-Optional outputpath and one or more input source file(s).
+#       If not specified a "L1Compiler.env" file must define OUTDIR and SOURCEFILE.
 # ==============================================================================
 
-JAR_PATH=./klangc.jar
-
+JAR_PATH=./klangc.jar           # The output JAR path and filename
 BUILD_ARTIFACTS_DIR=./build
-JAVA_SOURCES_ROOT=./src/main
+JAVA_SOURCES_ROOT=./src/main    # Parent folder of all java files that will be passed to javac
 
 javacOutDir=${BUILD_ARTIFACTS_DIR}/javac/
 sourcesListFile=${BUILD_ARTIFACTS_DIR}/sources.txt
-
 mkdir -p "${BUILD_ARTIFACTS_DIR}" "${javacOutDir}"
+
+# make sure lexer/parser were generated
+$(dirname $0)/run_antlr.sh
 
 # create list of all java files to be compiled
 find -depth -wholename "${JAVA_SOURCES_ROOT}/*.java" > ${sourcesListFile}
