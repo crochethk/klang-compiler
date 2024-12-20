ea7f64f3c9293aa5a3e19a52594601bf0f32eca5
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-20 22:40:21 +0100

    WIP: adjust jar build script to use compile_java.sh

--- scripts/build_compiler_jar.sh
+++ scripts/build_compiler_jar.sh
@@ -10,35 +10,30 @@
 #       If not specified a "klangc.env" file must define OUTDIR and SOURCEFILE.
 # ==============================================================================
 
-BUILD_ARTIFACTS_DIR=./build
-JAR_PATH=${BUILD_ARTIFACTS_DIR}/klangc.jar  # The output JAR path and filename
-JAVA_SOURCES_ROOT=./src/main                # Parent folder of all java files that will be passed to javac
+source ./scripts/compile_java.sh
 
-javacOutDir=${BUILD_ARTIFACTS_DIR}/jar/classes/
-sourcesListFile=${BUILD_ARTIFACTS_DIR}/jar/sources.txt
-mkdir -p "${BUILD_ARTIFACTS_DIR}" "${javacOutDir}"
+work_dir="${BUILD_ARTIFACTS_BASE_DIR}/jar"
+classes_out_dir="${work_dir}/classes/"
 
-# make sure lexer/parser were generated
-$(dirname $0)/run_antlr.sh
+# Make sure lexer and parser were generated
+./scripts/run_antlr.sh
 
-# create list of all java files to be compiled
-find "${JAVA_SOURCES_ROOT}" -type f -name "*.java" > ${sourcesListFile}
-
-# compile all files specified in "${sourcesListFile}"
-javac --enable-preview --source 23 --target 23 -cp "lib/antlr4-4.13.2-complete.jar:" -d "${javacOutDir}" @"${sourcesListFile}"
-
+# Compile compiler and copy to workdir
+compile_release
 if [ $? -ne 0 ]; then
     echo -e ">>> ERROR while compiling java source files\n"
     exit 1
 fi
+cp -r "${BUILD_ARTIFACTS_BASE_DIR}/release" "${work_dir}"
 
-# extract libraries to be included
+# Extract libraries to be included
 cwd_bak=$(pwd)
-cd "${javacOutDir}"
+cd "${classes_out_dir}"
 jar xf "${cwd_bak}/lib/antlr4-4.13.2-complete.jar"
 cd "${cwd_bak}"
 
-# create jar containing all class files, using the specified main class
-jar --create --file "${JAR_PATH}" --main-class cc.crochethk.compilerbau.praktikum.KlangCompiler -C "${javacOutDir}" .
-
-echo "Jar created: '${JAR_PATH}'"
+# Create jar containing all class files, using the specified main class
+# jar path and filename
+jar_path="${BUILD_ARTIFACTS_BASE_DIR}/klangc.jar"
+jar --create --file "${jar_path}" --main-class cc.crochethk.compilerbau.praktikum.KlangCompiler -C "${classes_out_dir}" .
+echo "Jar created: '${jar_path}'"
