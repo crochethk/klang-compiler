c7a792bc49b8a4ac3dbe4eeaefd601a740634292
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-23 20:57:28 +0100

    (Rename Parameter "params" to "fields")

--- src/main/java/cc/crochethk/compilerbau/praktikum/ast/StructDef.java
+++ src/main/java/cc/crochethk/compilerbau/praktikum/ast/StructDef.java
@@ -8,12 +8,12 @@ import utils.SourcePos;
 
 public class StructDef extends Node {
     public String name;
-    public List<Parameter> params;
+    public List<Parameter> fields;
 
-    public StructDef(SourcePos srcPos, String name, List<Parameter> params) {
+    public StructDef(SourcePos srcPos, String name, List<Parameter> fields) {
         super(srcPos);
         this.name = name;
-        this.params = params != null ? params : Collections.emptyList();
+        this.fields = fields != null ? fields : Collections.emptyList();
     }
 
     @Override
