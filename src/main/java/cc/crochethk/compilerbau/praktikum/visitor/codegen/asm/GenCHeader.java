7d2f7f667c058611b7769b44036b6c6fc8007ae5
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-12-25 06:15:25 +0100

    Basic setup for C helper code generation

--- src/main/java/cc/crochethk/compilerbau/praktikum/visitor/codegen/asm/GenCHeader.java
+++ src/main/java/cc/crochethk/compilerbau/praktikum/visitor/codegen/asm/GenCHeader.java
@@ -11,16 +11,26 @@ import cc.crochethk.compilerbau.praktikum.visitor.codegen.CodeGenVisitor;
 /** Generates a C header file containing all function signatures of the programs FunDef nodes. */
 public class GenCHeader extends CodeGenVisitor<Void> {
     private static final String FILE_EXT = ".h";
-    private SourceCodeBuilder scb;
+    private SourceCodeBuilder header;
+    private SourceCodeBuilder ccode;
 
     public GenCHeader(String outputDir, String packageName, String className) throws IOException {
         super(outputDir, packageName, className);
-        scb = new SourceCodeBuilder("  ");
+        header = new SourceCodeBuilder("  ");
+        ccode = new SourceCodeBuilder("  ");
     }
 
     @Override
     public Path outFilePath() {
-        return Path.of(outDir, packageName + "." + className + FILE_EXT);
+        return Path.of(outFilePathNoExt() + FILE_EXT);
+    }
+
+    private String outFilePathNoExt() {
+        return Path.of(outDir, fileNameNoExt()).toString();
+    }
+
+    private String fileNameNoExt() {
+        return packageName + "." + className;
     }
 
     @Override
@@ -105,53 +115,53 @@ public class GenCHeader extends CodeGenVisitor<Void> {
 
     @Override
     public Void visit(TypeNode type) {
-        scb.write(type.theType.cTypeName());
+        header.write(type.theType.cTypeName());
         return null;
     }
 
     @Override
     public Void visit(FunDef funDef) {
-        scb.write("\n");
+        header.write("\n");
         // Write return type
         funDef.returnType.accept(this);
-        scb.write(" ");
+        header.write(" ");
 
         // Write function name
-        scb.write(funDef.name, "(");
+        header.write(funDef.name, "(");
 
         // Add parameters
         if (funDef.params.isEmpty()) {
-            scb.write("void");
+            header.write("void");
         } else {
             for (int i = 0; i < funDef.params.size(); i++) {
                 var p = funDef.params.get(i);
                 // Write parameter type and name
                 p.type().accept(this);
-                scb.write(" ", p.name());
+                header.write(" ", p.name());
                 if (i < funDef.params.size() - 1)
-                    scb.write(", ");
+                    header.write(", ");
             }
         }
-        scb.write(");");
+        header.write(");");
         return null;
     }
 
     @Override
     public Void visit(StructDef structDef) {
-        scb.write("\nstruct ");
-        scb.write(structDef.name);
-        scb.write(" {");
+        header.write("\nstruct ");
+        header.write(structDef.name);
+        header.write(" {");
         for (int i = 0; i < structDef.fields.size(); i++) {
-            scb.writeIndented("");
+            header.writeIndented("");
             var f = structDef.fields.get(i);
             // Write field type and name
             f.type().accept(this);
-            scb.write(" ", f.name());
-            scb.write(";");
+            header.write(" ", f.name());
+            header.write(";");
         }
         if (!structDef.fields.isEmpty())
-            scb.write("\n");
-        scb.write("};\n");
+            header.write("\n");
+        header.write("};\n");
         return null;
     }
 
@@ -159,23 +169,35 @@ public class GenCHeader extends CodeGenVisitor<Void> {
     public Void visit(Prog prog) {
         var outputFileName = outFilePath().getFileName().toString();
         var guardName = outputFileName.replace('.', '_').toUpperCase();
-        scb.write("// Auto-generated C header file");
-        scb.write("\n#ifndef ", guardName);
-        scb.write("\n#define ", guardName, "\n");
-        scb.write("\n#include <stdint.h>");
-        scb.write("\n#include <stdbool.h>", "\n");
+        // H preamble
+        header.write("// Auto-generated C header file");
+        header.write("\n#ifndef ", guardName);
+        header.write("\n#define ", guardName, "\n");
+        header.write("\n#include <stdint.h>");
+        header.write("\n#include <stdbool.h>", "\n");
 
-        // Declare structs
-        prog.structDefs.forEach(st -> scb.write("\nstruct ", st.name, ";"));
-        scb.write("\n");
+        // C preamble
+        ccode.write("#include \"", fileNameNoExt(), ".h\"", "\n");
+
+        // Declare structs in header
+        prog.structDefs.forEach(st -> header.write("\nstruct ", st.name, ";"));
+        header.write("\n");
+
+        // Declare and define funcitons
         prog.funDefs.forEach(f -> f.accept(this));
-        // Write actual struct definitions
+        // Declare and define structs
         prog.structDefs.forEach(st -> st.accept(this));
 
-        scb.write("\n#endif // ", guardName, "\n");
+        header.write("\n#endif // ", guardName, "\n");
 
-        try (var w = new FileWriter(outFilePath().toFile())) {
-            w.write(scb.toString());
+        // Dump source code to files
+        try {
+            var h = new FileWriter(outFilePathNoExt() + ".h");
+            h.write(header.toString());
+            h.close();
+            var c = new FileWriter(outFilePathNoExt() + ".c");
+            c.write(ccode.toString());
+            c.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
