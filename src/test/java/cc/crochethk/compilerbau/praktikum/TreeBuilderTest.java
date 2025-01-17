1d02b9aa6ec2714de4a2f8d4e57b260471fc0658
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-01-17 15:25:06 +0100

    Fix TreeBuilderTest to actually walk the parsetree recursively (ctx is populated properly)

--- src/test/java/cc/crochethk/compilerbau/praktikum/TreeBuilderTest.java
+++ src/test/java/cc/crochethk/compilerbau/praktikum/TreeBuilderTest.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.InputMismatchException;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
@@ -33,15 +34,16 @@ public class TreeBuilderTest extends NodeMocker {
         }
     }
 
-    private <C extends ParserRuleContext> C parse(String s, Function<KlangParser, C> contextGetter) {
+    private <C extends ParserRuleContext> C parseAndWalk(String s, Function<KlangParser, C> contextGetter) {
         var lexer = KlangCompiler.buildLexer(CharStreams.fromString(s));
-        return contextGetter.apply(KlangCompiler.buildParser(lexer));
+        var ctx = contextGetter.apply(KlangCompiler.buildParser(lexer));
+        ParseTreeWalker.DEFAULT.walk(treeBuilder, ctx);
+        return ctx;
     }
 
     @Test
     void testExitNullLit() {
-        var ctx = parse("null", p -> p.nullLit());
-        treeBuilder.exitNullLit(ctx);
+        var ctx = parseAndWalk("null", p -> p.nullLit());
         assertEquals(NULL_LIT, ctx.result);
     }
 
@@ -83,43 +85,37 @@ public class TreeBuilderTest extends NodeMocker {
 
         @Test
         public void buildWithMixedEscapeSequences() {
-            var ctx = parse("\"esc. \\\\\\quote \\\" inside\\n String\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\"esc. \\\\\\quote \\\" inside\\n String\"", p -> p.string());
             assertEquals(stringLit("esc. \\uote \" inside\n String"), ctx.result);
         }
 
         @Test
         public void buildBasicStringLit() {
-            var ctx = parse("\"hello world\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\"hello world\"", p -> p.string());
             assertEquals(stringLit("hello world"), ctx.result);
         }
 
         @Test
         public void buildEmptyStringLit() {
-            var ctx = parse("\"\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\"\"", p -> p.string());
             assertEquals(stringLit(""), ctx.result);
         }
 
         @Test
         public void buildMultiLineStringLit() {
-            var ctx = parse("\"Hello\nWorld\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\"Hello\nWorld\"", p -> p.string());
             assertEquals(stringLit("Hello\nWorld"), ctx.result);
         }
 
         @Test
         public void buildEmptyMultiLineStringLit() {
-            var ctx = parse("\"\n\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\"\n\"", p -> p.string());
             assertEquals(stringLit("\n"), ctx.result);
         }
 
         @Test
         public void buildUnescapedWhitespaceStringLit() {
-            var ctx = parse("\" \r\t\n\"", p -> p.string());
-            treeBuilder.exitString(ctx);
+            var ctx = parseAndWalk("\" \r\t\n\"", p -> p.string());
             assertEquals(stringLit(" \r\t\n"), ctx.result);
         }
 
@@ -129,15 +125,13 @@ public class TreeBuilderTest extends NodeMocker {
     class ExitBoolTests {
         @Test
         public void buildBoolLitTrue() {
-            var ctx = parse("true", p -> p.bool());
-            treeBuilder.exitBool(ctx);
+            var ctx = parseAndWalk("true", p -> p.bool());
             assertEquals(boolLit(true), ctx.result);
         }
 
         @Test
         public void buildBoolLitFalse() {
-            var ctx = parse("false", p -> p.bool());
-            treeBuilder.exitBool(ctx);
+            var ctx = parseAndWalk("false", p -> p.bool());
             assertEquals(boolLit(false), ctx.result);
         }
     }
@@ -146,44 +140,38 @@ public class TreeBuilderTest extends NodeMocker {
     class ExitNumberTests {
         @Test
         public void buildI64Lit() {
-            var ctx = parse("123", p -> p.number());
-            treeBuilder.exitNumber(ctx);
+            var ctx = parseAndWalk("123", p -> p.number());
             assertEquals(i64Lit(123, false), ctx.result);
         }
 
         @Test
         public void buildI64LitWithTypeAnnot() {
-            var ctx = parse("123 as i64", p -> p.number());
-            treeBuilder.exitNumber(ctx);
+            var ctx = parseAndWalk("123 as i64", p -> p.number());
             assertEquals(i64Lit(123, true), ctx.result);
         }
 
         @Test
         public void buildF64Lit() {
-            var ctx = parse("1.23", p -> p.number());
-            treeBuilder.exitNumber(ctx);
+            var ctx = parseAndWalk("1.23", p -> p.number());
             assertEquals(f64Lit(1.23d, false), ctx.result);
         }
 
         @Test
         public void buildF64LitWithTypeAnnot() {
-            var ctx = parse("1.23 as f64", p -> p.number());
-            treeBuilder.exitNumber(ctx);
+            var ctx = parseAndWalk("1.23 as f64", p -> p.number());
             assertEquals(f64Lit(1.23d, true), ctx.result);
         }
 
         @Test
         public void buildF64LitWithTypeAnnotFromI64() {
-            var ctx = parse("123 as f64", p -> p.number());
-            treeBuilder.exitNumber(ctx);
+            var ctx = parseAndWalk("123 as f64", p -> p.number());
             assertEquals(f64Lit(123.0d, true), ctx.result);
         }
 
         @Test
         public void castFloatLiteralToInteger_throwsIllegalLiteralTypeAnnotException() {
-            var ctx = parse("123.456 as i64", p -> p.number());
             Exception exception = assertThrows(IllegalLiteralTypeAnnotException.class, () -> {
-                treeBuilder.exitNumber(ctx);
+                parseAndWalk("123.456 as i64", p -> p.number());
             });
             assertTrue(exception.getMessage().contains("Illegal type suffix"));
         }
@@ -195,8 +183,7 @@ public class TreeBuilderTest extends NodeMocker {
 
         @Test
         public void buildDropStat() {
-            var ctx = parse("drop someVarName;", p -> p.statement());
-            treeBuilder.exitStatement(ctx);
+            var ctx = parseAndWalk("drop someVarName;", p -> p.statement());
             assertEquals(dropStat("someVarName"), ctx.result);
         }
 
@@ -204,7 +191,7 @@ public class TreeBuilderTest extends NodeMocker {
         @Test
         void buildDropWithoutNamedVarShouldThrow() {
             var _ = assertThrows(InputMismatchException.class, () -> {
-                parse("drop getSomeRef();", p -> p.statement());
+                parseAndWalk("drop getSomeRef();", p -> p.statement());
             });
         }
     }
